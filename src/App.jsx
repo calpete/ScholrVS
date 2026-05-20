a0ae2e6c86152131cce885d9ae755584a03ea2c6
Callum Peterson <cal@takemobi.com>
2026-05-20 10:45:01 -0400

    debug student notes upload

--- src/App.jsx
+++ src/App.jsx
@@ -1067,6 +1067,9 @@ function StudentView({ course, documents, suggestedQuestions, onExit, studentTok
     fd.append('file', file);
     try {
       await fetch(`${API}/student/notes/${course.id}/upload`, { method: 'POST', headers: authHeaders, body: fd });
+      const data = await res.json();
+console.log('Note upload response:', data);
+    
       const reader = new FileReader();
       reader.onload = (e) => {
         const ext = file.name.toLowerCase().split('.').pop();
